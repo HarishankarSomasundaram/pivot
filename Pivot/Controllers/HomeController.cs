26eba45d590c8e4105d1a867e759f550dc4ad539
harishankarsomasundaram <harishankar.info@gmail.com>
2014-07-26 00:37:42 +0530

    init

---
+++ Pivot/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevExpress.Web.Mvc;
+
+namespace Pivot.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Demo()
+        {
+            return View();
+        }
+
+        Pivot.Models.NWindEntities db = new Pivot.Models.NWindEntities();
+
+
+
+        [ValidateInput(false)]
+        public ActionResult PivotGrid2Partial()
+        {
+            var model = db.Invoices;
+            return PartialView("_PivotGrid2Partial", model.ToList());
+        }
+
+        [ValidateInput(false)]
+        public ActionResult PivotGrid3Partial()
+        {
+            var model = new object[0];
+            return PartialView("_PivotGrid3Partial", model);
+        }
+	}
+}
